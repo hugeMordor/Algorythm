8fde755c2895176fbfcc8d11e3cbb05b80af6783
hugeMordor <72040616+hugeMordor@users.noreply.github.com>
2021-05-05 20:07:43 +0300

    Add files via upload

--- Algorythm/Form1.Designer.cs
+++ Algorythm/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -112,11 +117,60 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Кол-во выбираемых";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(269, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Новый";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(29, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(29, 157);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Старый";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 164);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Новый";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 377);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
@@ -142,6 +196,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Algorythm/Form1.cs
+++ Algorythm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Algorythm
 {
@@ -19,7 +20,7 @@ namespace Algorythm
         char[] A;
         int[] C;
         char ch;
-        string st;
+        string st, str;
         char[] AA;
         int[] CC;
 
@@ -30,6 +31,8 @@ namespace Algorythm
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             n = int.Parse(textBox1.Text);
             A = new char[n];
             C = new int[n];
@@ -56,7 +59,8 @@ namespace Algorythm
             C[k] = 0;
             k--;
             if (k >= 0) goto L2;
-            //koneц
+            stopwatch.Stop();
+            textBox3.Text = Convert.ToString(stopwatch.ElapsedTicks);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -132,6 +136,160 @@ namespace Algorythm
             k--;
             if (k >= 0) goto LL2;
             for (int i = 0; i < n; i++) AA[i] = (char)(65 + i);
+        }
+
+        public class Permutations
+        {
+            //Список перестановок
+            private List<string> _permutationsList;
+            private String _str;
+
+            /// <summary>
+            /// Добавляет новую перестановку в список
+            /// </summary>
+            /// <param name="a">Массив символов
+            /// <param name="repeat">Содержать повторы
+            private void AddToList(char[] a, bool repeat = true)
+            {
+                var bufer = new StringBuilder("");
+                for (int i = 0; i < a.Count(); i++)
+                {
+                    bufer.Append(a[i]);
+                }
+                if (repeat || !_permutationsList.Contains(bufer.ToString()))
+                {
+                    _permutationsList.Add(bufer.ToString());
+                }
+
+            }
+
+            /// <summary>
+            /// Рекурсивный поиск всех перестановок
+            /// </summary>
+            /// <param name="a">
+            /// <param name="n">
+            /// <param name="repeat">Содержать повторы
+            private void RecPermutation(char[] a, int n, bool repeat = true)
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    var temp = a[n - 1];
+                    for (var j = n - 1; j > 0; j--)
+                    {
+                        a[j] = a[j - 1];
+                    }
+                    a[0] = temp;
+                    if (i < n - 1) AddToList(a, repeat);
+                    if (n > 0) RecPermutation(a, n - 1, repeat);
+                }
+            }
+
+            public Permutations()
+            {
+                _str = "";
+            }
+
+            public Permutations(String str)
+            {
+                _str = str;
+            }
+            /// <summary>
+            /// Строка, на основе которой строятся перестановки
+            /// </summary>
+            public String PermutationStr
+            {
+                get
+                {
+                    return _str;
+                }
+                set
+                {
+                    _str = value;
+                }
+            }
+            /// <summary>
+            /// Получает список всех перестановок
+            /// </summary>
+            /// <param name="repeat">Содержать повторения
+            /// <returns></returns>
+            public List<string> GetPermutationsList(bool repeat = true)
+            {
+                _permutationsList = new List<string> { _str };
+                RecPermutation(_str.ToArray(), _str.Length, repeat);
+                return _permutationsList;
+            }
+            /// <summary>
+            /// Получает отсортированный список всех перестановок
+            /// </summary>
+            /// <param name="repeat">Содержать повторения
+            /// <returns></returns>
+            public List<string> GetPermutationsSortList(bool repeat = true)
+            {
+                GetPermutationsList(repeat).Sort();
+                return _permutationsList;
+            }
+
+        }
+
+        //private void button5_Click(object sender, EventArgs e)
+        //{
+        //    listBox1.Items.Clear();
+        //    n = int.Parse(textBox1.Text);
+        //    A = new char[n];
+        //    C = new int[n];
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        str += (char)(65 + i);
+        //    }
+        //    var per = new Permutations(str);
+        //    var list = per.GetPermutationsSortList();
+
+        //    foreach (var l in list)
+        //    {
+        //        listBox1.Items.Add(l);
+        //    }
+        //}
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+        //    listBox1.Items.Clear();
+        //    n = int.Parse(textBox1.Text);
+        //    A = new char[n];
+        //    C = new int[n];
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        str += (char)(65 + i);
+        //    }
+        //    var per = new Permutations(str);
+        //    var list = per.GetPermutationsSortList(false);
+
+        //    foreach (var l in list)
+        //    {
+        //        listBox1.Items.Add(l);
+        //    }
+        //}
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            listBox1.Items.Clear();
+            n = int.Parse(textBox1.Text);
+            A = new char[n];
+            C = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                str += (char)(65 + i);
+            }
+            var per = new Permutations(str);
+            var list = per.GetPermutationsList();
+
+            foreach (var l in list)
+            {
+                listBox1.Items.Add(l);
+            }
+            stopwatch.Stop();
+            textBox4.Text = Convert.ToString(stopwatch.ElapsedTicks);
         }
     }
 }
